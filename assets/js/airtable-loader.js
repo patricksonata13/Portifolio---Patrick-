a7ba7902fa784282622e7fa49175a9c659427e64
patricksonata13 <ick12reis@gmail.com>
2026-02-13 15:45:09 -0300

    Restaura chave do Airtable

--- assets/js/airtable-loader.js
+++ assets/js/airtable-loader.js
@@ -5,7 +5,7 @@
     console.log('🔍 Iniciando carregamento do Airtable...');
     
     // COLE SUA CHAVE AQUI (substitua pela sua)
-    const API_KEY = "SUA_CHAVE_AQUI"; // Substitua pela sua chave
+    const API_KEY = "pat6YwiDlLsOmEEfl.3f66193f5344f7ed173e5172132ebbc7219ce690b2c9dbcda07ed319d548b04a"; // Substitua pela sua chave
     const BASE_ID = 'app1lUWbmq98n18cA';
     
     fetch(`https://api.airtable.com/v0/${BASE_ID}/Projetos`, {
