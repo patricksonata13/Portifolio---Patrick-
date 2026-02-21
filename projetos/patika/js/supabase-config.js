f85ce5d48a659bcf9fdd7780a0755e46269b3880
patricksonata13 <ick12reis@gmail.com>
2026-02-21 17:58:16 -0300

    organiza estrutura do Patika e remove arquivos duplicados

--- projetos/patika/js/supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Configurações do Supabase - PATIKA
-const SB_URL = "SUA_URL_AQUI";
-const SB_KEY = "SUA_CHAVE_ANON_AQUI";
-
-// Inicialização do Cliente (Script injetado via CDN no HTML)
-const supabase = supabase.createClient(SB_URL, SB_KEY);
